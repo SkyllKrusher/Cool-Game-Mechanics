af985f60ff80081095d54ac490807dc4d98954f1
AyushBharwdaj <bhardwaj.ayush007@gmail.com>
2020-07-22 05:17:47 +0530

    2d platmorver movement empty script created

---
+++ Assets/Scripts/2D Platformer Basic Movement/PlayerMovement2DPlatformer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoolMechanics.PlayerMovement2DPlatformer
+{
+    public class PlayerMovement2DPlatformer : MonoBehaviour
+    {
+
+    }
+}
